c5f1245897bade1bbd30ac44f0c2af05e33133ce
joeynovak <joey.novak@gmail.com>
2019-03-07 21:19:06 -0800

    Working on Logo

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <img id="logo" src="~@/assets/logo.png" alt="reaktor-charger">
     <main>
       <div class="left-side">
         <span class="title">
           Welcome to the Reaktor Charger Logger
         </span>
-        <system-information></system-information>
       </div>
 
       <div class="right-side">
